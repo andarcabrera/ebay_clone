3041e272b2958e6582c50e0f64b93ddb8f959468
Anda <andarcabrera@gmail.com>
2016-05-17 11:41:06 -0500

    delete text limit for text area in migration

--- db/migrate/20160510135800_create_items.rb
+++ db/migrate/20160510135800_create_items.rb
@@ -1,8 +1,8 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-      t.string   :name, null: false, limit: 50
-      t.text     :description, null: false, limit: 250
+      t.string   :name, null: false
+      t.text     :description, null: false
       t.integer  :price, null: false
       t.string   :email, null: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,10 @@ ActiveRecord::Schema.define(version: 20160511124800) do
   enable_extension "plpgsql"
 
   create_table "items", force: :cascade do |t|
-    t.string   "name",        limit: 50, null: false
-    t.text     "description",            null: false
-    t.integer  "price",                  null: false
-    t.string   "email",                  null: false
+    t.string   "name",        null: false
+    t.text     "description", null: false
+    t.integer  "price",       null: false
+    t.string   "email",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
