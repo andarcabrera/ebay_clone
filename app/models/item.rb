347f34cbc4fd3147bee8df7647dc453634722154
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-05-19 10:36:03 -0500

    remove before_create condition for item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,5 @@
 class Item < ActiveRecord::Base
   validates :name, :description, :price, :email,  presence: true
   mount_uploader :image, ImageUploader
-
-  before_create :make_available
-
-  def make_available
-    self.available ||= true
-  end
 end
 
