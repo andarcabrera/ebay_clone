7c3e6e44c2b8e67a1cb8881aa31ca598fc4e601c
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-05-23 16:34:18 -0500

    add users controller new and create actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-
   root 'items#index'
   get  "/items/new"                         =>  "items#new"
   post "/items"                             =>  "items#create"
   get  "/items/:item_id/purchases/new"      =>  "purchases#new", as: 'new_purchase'
   post "/items/:item_id/purchases"          =>  "purchases#create"
   get  "/items/:item_id/purchases/:id"      =>  "purchases#show"
+  get  "/users/new"                         =>  "users#new"
+  post "/users"                             =>  "users#create"
 end
