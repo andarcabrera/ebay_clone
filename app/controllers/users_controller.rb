934a7c52ec9db47491600bd03405452bc3ed03a6
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-05-24 09:24:35 -0500

    add presenter for new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
+require 'users/presenters/new_user_presenter'
+
 class UsersController < ApplicationController
   def new
+    @presenter = NewUserPresenter.new(User.new)
   end
 
   def create
     user = User.new(user_params)
+    @presenter = NewUserPresenter.new(user)
     if user.valid?
       user.save
       redirect_to :root
