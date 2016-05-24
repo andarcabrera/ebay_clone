508b7000cd30454d7ba50bf8b106c5c58c6fdfb2
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-05-24 15:12:15 -0500

    Update routes with session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'items#index'
+  root "sessions#new"
+  get  "/items"                             =>  "items#index"
   get  "/items/new"                         =>  "items#new"
   post "/items"                             =>  "items#create"
   get  "/items/:item_id/purchases/new"      =>  "purchases#new", as: 'new_purchase'
@@ -7,4 +8,7 @@ Rails.application.routes.draw do
   get  "/items/:item_id/purchases/:id"      =>  "purchases#show"
   get  "/users/new"                         =>  "users#new"
   post "/users"                             =>  "users#create"
+  get  "/login"                             =>  "sessions#new"
+  post "/login"                             =>  "sessions#create"
+  get  "/logout"                            =>  "sessions#destroy"
 end
