719198bb8e7dd1f9caea3b9b32675c8006e298d7
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-05-24 15:15:39 -0500

    Add session controller and bcrypt authentication

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,146 @@
+describe SessionsController do
+  context "request for login page" do
+    it "has a successful response" do
+      get :new
+
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq("text/html")
+    end
+
+    it "renders the new session template" do
+      get :new
+
+      expect(response).to render_template(:new)
+    end
+  end
+
+  context "login successful" do
+    before(:each) do
+      User.create(username: "chosen", email: "thechosenone@example.com", password: "guitarman")
+    end
+
+    it "has a successful response" do
+      post :create, session: { email: "thechosenone@example.com", password: "guitarman" }
+
+      expect(response).to have_http_status(:redirect)
+      expect(response.content_type).to eq("text/html")
+    end
+
+    it "redirects to the item index page" do
+      post :create, session: { email: "thechosenone@example.com", password: "guitarman" }
+
+      expect(response).to redirect_to items_path
+    end
+
+    it "creates a session for the user" do
+      post :create, session: { email: "thechosenone@example.com", password: "guitarman" }
+
+      expect(session[:user_id]).to eq(User.last.id)
+    end
+
+    it "does not show a flash message" do
+      post :create, session: { email: "thechosenone@example.com", password: "guitarman" }
+
+      expect(flash[:notice]).to be_nil
+    end
+  end
+
+  context "login is unsuccessful due to incorrect credentials" do
+    before(:each) do
+      User.create(username: "chosen", email: "thechosenone@example.com", password: "guitarman")
+    end
+
+    it "has a successful response" do
+      post :create, session: { email: "thechosenone@example.com", password: "ukaleileman" }
+
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq("text/html")
+    end
+
+    it "renders the login page" do
+      post :create, session: { email: "thechosenone@example.com", password: "tambourineman" }
+
+      expect(response).to render_template(:new)
+    end
+
+    it "shows a flash message" do
+      post :create, session: { email: "thechosenone@example.com", password: "tambourineman" }
+
+      expect(flash[:notice]).to eq("Incorrect email/password combination. You are on a secure website my friend.")
+    end
+
+    it "does not create a session for the user" do
+      post :create, session: { email: "thechosenone@example.com", password: "tambourineman" }
+
+      expect(session[:user_id]).to be_nil
+    end
+  end
+
+  context "login is unsuccessful due to incomplete credentials" do
+    context "no email provided" do
+      it "renders login page" do
+        post :create, session: { password: "ukaleileman" }
+
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to eq("text/html")
+        expect(response).to render_template(:new)
+      end
+
+      it "shows a flash message" do
+        post :create, session: { password: "ukaleileman" }
+
+        expect(flash[:notice]).to eq("Incorrect email/password combination. You are on a secure website my friend.")
+      end
+
+      it "does not create a session for the user" do
+        post :create, session: { password: "ukaleileman" }
+
+        expect(session[:user_id]).to be_nil
+      end
+    end
+
+    context "no password provided" do
+      it "renders login page" do
+        post :create, session: { email: "thechosenone@example.com" }
+
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to eq("text/html")
+        expect(response).to render_template(:new)
+      end
+
+      it "shows a flash message" do
+        post :create, session: { email: "thechosenone@example.com" }
+
+        expect(flash[:notice]).to eq("Incorrect email/password combination. You are on a secure website my friend.")
+      end
+
+      it "does not create a session" do
+        post :create, session: { email: "thechosenone@example.com" }
+
+        expect(session[:user_id]).to be_nil
+      end
+    end
+
+    context "user successfully logs out" do
+      before(:each) do
+        User.create(username: "chosen", email: "thechosenone@example.com", password: "guitarman")
+      end
+
+      it "redirects to the items index" do
+        post :create, session: { email: "thechosenone@example.com", password: "guitarman" }
+        delete :destroy
+
+        expect(response).to have_http_status(:redirect)
+        expect(response.content_type).to eq("text/html")
+        expect(response).to redirect_to items_path
+      end
+
+      it "deletes the session" do
+        post :create, session: { email: "thechosenone@example.com", password: "guitarman" }
+        delete :destroy
+
+        expect(session[:user_id]).to be_nil
+      end
+    end
+  end
+end
