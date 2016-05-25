1569d3261d09178603d27d87eafb8cfb6e1bf161
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-05-25 16:06:07 -0500

    precompile assets

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,21 @@ require 'faker'
 
 images = Dir.entries(File.open(File.join(Rails.root, "public/sample_images/"))) - [".", ".."]
 
-10.times do
+500.times do
   item =  Item.find_or_create_by(name: Faker::Lorem.word,
                          description: Faker::Lorem.paragraph,
                          price: Faker::Number.number(2),
-                         email: Faker::Internet.email)
+                         email: "jeromecharles@example.com")
+
+  item.image.store!(File.open(File.join(Rails.root, File.join("public/sample_images/", images.sample))))
+  item.save
+end
+
+500.times do
+  item =  Item.find_or_create_by(name: Faker::Lorem.word,
+                         description: Faker::Lorem.paragraph,
+                         price: Faker::Number.number(2),
+                         email: "andaraluca@example.com")
 
   item.image.store!(File.open(File.join(Rails.root, File.join("public/sample_images/", images.sample))))
   item.save
