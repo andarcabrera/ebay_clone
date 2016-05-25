d8f63c867d336e0f18027c664879b6ecdaf6a69c
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-05-25 15:44:14 -0500

    update mailer and job after changing associations

--- spec/mailers/item_sold_mailer_spec.rb
+++ spec/mailers/item_sold_mailer_spec.rb
@@ -1,33 +1,36 @@
-require 'rails_helper'
-require 'spec_helper'
+require "rails_helper"
+require "spec_helper"
 
 describe ItemSoldMailer do
-  context 'an item is sold' do
-    let(:purchase) { double 'purchase', email: 'sternmother@email.com', item_id: 1 }
-    let(:item) { double 'item', name: 'phone', description: 'it rings', price: 54, email: 'seller@me.com' }
+  context "an item is sold" do
+    let (:seller) { double "seller", id: 3, email: "hey@you.com" }
+    let (:purchaser) { double "purchaser", id: 3, email: "areyouthere@email.com" }
+    let (:purchase) { double "purchase", id: 3, purchaser: purchaser, purchaser_id: purchaser.id, item_id: item.id }
+    let (:item) { double "item", id: 3, seller: seller, name: "phone", description: "smart", price: 3, seller_id: seller.id }
+    let (:presenter) { NewPurchasePresenter.new(purchase, item) }
     let(:mail) { described_class.notify_seller(purchase, item).deliver_now }
 
-    it 'it has a subject' do
+    it "it has a subject" do
 
-      expect(mail.subject).to eq('Item Sold')
+      expect(mail.subject).to eq("Item Sold")
     end
 
-    it 'it is sent to the item\'s seller email' do
+    it "it is sent to the item's seller email" do
 
-      expect(mail.to).to eq(['seller@me.com'])
+      expect(mail.to).to eq(["hey@you.com"])
     end
 
-    it 'it is sent from the ebay_clone default email' do
+    it "it is sent from the ebay_clone default email" do
 
-      expect(mail.from).to eq(['ebaycloneacjg@gmail.com'])
+      expect(mail.from).to eq(["ebaycloneacjg@gmail.com"])
     end
 
-    it 'the body of the email contains the email of the buyer' do
+    it "the body of the email contains the email of the buyer" do
 
-      expect(mail.body).to include(purchase.email)
+      expect(mail.body).to include(purchase.purchaser.email)
     end
 
-    it 'the body of the email contains the item details' do
+    it "the body of the email contains the item details" do
 
       expect(mail.body).to include(item.name)
       expect(mail.body).to include(item.price)
