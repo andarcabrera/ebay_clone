5b989692049935f8fdc37689bf4154ef38e76d7f
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-05-26 09:23:22 -0500

    Add cache dir to uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,4 +4,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
 end
