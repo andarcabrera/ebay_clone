f88c80d399ba1fda00511b17805c1f0e9f9883ea
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-05-26 09:15:11 -0500

    Move sample images to assets folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
 require 'faker'
 
-images = Dir.entries(File.open(File.join(Rails.root, "public/sample_images/"))) - [".", ".."]
+images = Dir.entries(File.open(File.join(Rails.root, "app/assets/images/sample_images/"))) - [".", ".."]
 
-500.times do
+5.times do
   item =  Item.find_or_create_by(name: Faker::Lorem.word,
                          description: Faker::Lorem.paragraph,
                          price: Faker::Number.number(2),
                          email: "jeromecharles@example.com")
 
-  item.image.store!(File.open(File.join(Rails.root, File.join("public/sample_images/", images.sample))))
+  item.image.store!(File.open(File.join(Rails.root, File.join("app/assets/images/sample_images/", images.sample))))
   item.save
 end
 
-500.times do
+5.times do
   item =  Item.find_or_create_by(name: Faker::Lorem.word,
                          description: Faker::Lorem.paragraph,
                          price: Faker::Number.number(2),
                          email: "andaraluca@example.com")
 
-  item.image.store!(File.open(File.join(Rails.root, File.join("public/sample_images/", images.sample))))
+  item.image.store!(File.open(File.join(Rails.root, File.join("app/assets/images/sample_images/", images.sample))))
   item.save
 end
