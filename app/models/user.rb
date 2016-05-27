28f27958aa05e7c15844f9c5814ea3f3cedccdc1
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-05-27 09:20:56 -0500

    move class method above the instance methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,13 @@ class User < ActiveRecord::Base
   has_many :listings, class_name: "Item", foreign_key: :seller_id
   has_many :purchases, class_name: "Purchase", foreign_key: :purchaser_id
 
+  def self.authenticate(session_params)
+    user = User.find_by(email: session_params[:email])
+    if user && user.password == session_params[:password]
+      user
+    end
+  end
+
   def password
     @password ||= Password.new(password_hash)
   end
@@ -15,12 +22,5 @@ class User < ActiveRecord::Base
   def password=(new_password)
     @password = Password.create(new_password)
     self.password_hash = @password
-  end
-
-  def self.authenticate(session_params)
-    user = User.find_by(email: session_params[:email])
-    if user && user.password == session_params[:password]
-      user
-    end
   end
 end
