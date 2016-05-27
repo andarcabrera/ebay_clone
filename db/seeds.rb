aea9e94ba4a255549f8f08771c78278cadb959c1
Jerome Goodrich <jeromegoodrich@gmail.com>
2016-05-27 11:27:48 -0500

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 require 'faker'
 require 'bcrypt'
-include BCrypt
-images = Dir.entries(File.open(File.join(Rails.root, "public/sample_images/"))) - [".", ".."]
+
+images = Dir.entries(File.open(File.join(Rails.root, "app/assets/images/sample_images/"))) - [".", ".."]
 
 10.times do
   user = User.find_or_create_by(username: Faker::Lorem.word,
                                 email: Faker::Internet.email,
-                                password_hash: Password.create("1234password"))
+                                password_hash: BCrypt::Password.create("1234password"))
 
   item = user.listings.find_or_create_by(name: Faker::Lorem.word,
                          description: Faker::Lorem.paragraph,
