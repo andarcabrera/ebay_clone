0c68face7bd54bbcdb6d2055f725c6a2e825e690
Anda <andarcabrera@gmail.com>
2016-06-03 11:37:18 -0500

    refactor auction worker and add more tests

--- app/jobs/auction_worker.rb
+++ app/jobs/auction_worker.rb
@@ -2,11 +2,12 @@ class AuctionWorker
   include Sidekiq::Worker
 
   def perform(item_id)
-    Item.transaction do
-      item = Item.lock.find(item_id)
+    item = Item.find(item_id)
+    item.with_lock do
       if item.available && item.bids.count > 0
-        winning_bid = item.bids.find { |bid| bid.amount == item.highest_bid }
+        winning_bid = item.bids.find_by(amount: item.highest_bid)
         purchase = Purchase.create(item_id: item.id, purchaser_id: winning_bid.bidder_id)
+        item.update_attributes(available: false)
         ItemSoldMailer.notify_seller(purchase, item).deliver_now
         ItemSoldMailer.notify_after_auction(purchase, item).deliver_now
       end
