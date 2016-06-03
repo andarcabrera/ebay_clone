b698cf281ba9a062bd1147ce147423290b6af54b
Anda <andarcabrera@gmail.com>
2016-06-03 10:14:19 -0500

    add with_lock to bid controller

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -8,9 +8,9 @@ class BidsController < ApplicationController
   end
 
   def create
-    bid = Bid.new(item_id: params[:item_id], bidder_id: current_user.id, amount: params[:amount])
-    Item.transaction do
-      item = Item.lock.find(params[:item_id])
+    bid = Bid.new(item_id: bid_params[:item_id], bidder_id: current_user.id, amount: bid_params[:amount])
+    item = Item.find(params[:item_id])
+    item.with_lock do
       if bid.valid?
         bid.save
         redirect_to action: "show", item_id: bid.item_id, id: bid.id
@@ -24,5 +24,11 @@ class BidsController < ApplicationController
   def show
     bid = Bid.find(params[:id])
     @presenter = BidPresenter.new(bid)
+  end
+
+  private
+
+  def bid_params
+    params.permit(:amount, :item_id)
   end
 end
