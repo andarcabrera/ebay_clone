c7bd5ca495fe6180581f0fc2dd20d60af53c4158
Anda <andarcabrera@gmail.com>
2016-06-09 12:52:55 -0500

    add tests for countdown timer

--- spec/javascripts/countdown_spec.js
+++ spec/javascripts/countdown_spec.js
@@ -2,12 +2,12 @@
 //= require countdown
 
 function setFixtures(endTime) {
-      fixture.set('<p id="end_time">' + new Date(endTime) + '</p>'
-          + '<p id="days"></p>'
-          + '<p id="hours"></p>'
-          + '<p id="minutes"></p>'
-          + '<p id="seconds"></p>'
-          + '<p id="on-end-test">change on end</p>');
+  fixture.set('<p id="end_time">' + new Date(endTime) + '</p>'
+      + '<p id="days"></p>'
+      + '<p id="hours"></p>'
+      + '<p id="minutes"></p>'
+      + '<p id="seconds"></p>'
+      + '<p id="on-end-test">change on end</p>');
 }
 
 function initializeCountdown(endTime, trackingElements){
@@ -20,9 +20,9 @@ function initializeCountdown(endTime, trackingElements){
 
 function TrackingElements() {
   this.days = document.getElementById("days")
-  this.hours = document.getElementById("hours")
-  this.minutes = document.getElementById("minutes")
-  this.seconds = document.getElementById("seconds")
+    this.hours = document.getElementById("hours")
+    this.minutes = document.getElementById("minutes")
+    this.seconds = document.getElementById("seconds")
 }
 
 function setUpTests(endTime) {
@@ -36,6 +36,9 @@ function setUpTests(endTime) {
 describe("Coundown", function() {
   describe("auction is on going", function() {
     beforeEach(function(){
+      jasmine.clock().uninstall();
+      jasmine.clock().install();
+      jasmine.clock().mockDate(new Date());
       endTime = Date.now() + 237300000;
       setUpTests(endTime);
     });
@@ -46,6 +49,31 @@ describe("Coundown", function() {
       expect(document.getElementById("hours").innerHTML).toBe("17");
       expect(document.getElementById("minutes").innerHTML).toBe("54");
       expect(document.getElementById("seconds").innerHTML).toBe("59");
+
+    });
+
+    it("update after a second has passed", function() {
+      jasmine.clock().tick(1000);
+
+      expect(document.getElementById("seconds").innerHTML).toBe("58");
+    });
+
+    it("update after a minute has passed", function() {
+      jasmine.clock().tick(60000);
+
+      expect(document.getElementById("minutes").innerHTML).toBe("53");
+    });
+
+    it("update after an hour has passed", function() {
+      jasmine.clock().tick(3600000);
+
+      expect(document.getElementById("hours").innerHTML).toBe("16");
+    });
+
+    it("update after a day has passed", function() {
+      jasmine.clock().tick(86400000);
+
+      expect(document.getElementById("days").innerHTML).toBe("1");
     });
 
     it("does not modify the on end DOM element", function() {
