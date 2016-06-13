38ebfb243bfea7ce223da9528af2af9e2bbe64a8
Anda <andarcabrera@gmail.com>
2016-06-13 08:38:10 -0500

    add updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -45,12 +45,16 @@ ActiveRecord::Schema.define(version: 20160608171200) do
   end
 
   create_table "taggings", force: :cascade do |t|
-    t.integer "item_id", null: false
-    t.integer "tag_id",  null: false
+    t.integer  "item_id",    null: false
+    t.integer  "tag_id",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "name", null: false
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
