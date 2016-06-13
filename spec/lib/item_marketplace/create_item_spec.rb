645a20c0e0b1c6dad760a49fe6be438bc9eb3e1a
Anda <andarcabrera@gmail.com>
2016-06-13 14:04:32 -0500

    clean-up create_item_specs

--- spec/lib/item_marketplace/create_item_spec.rb
+++ spec/lib/item_marketplace/create_item_spec.rb
@@ -2,13 +2,17 @@ require 'item_marketplace/create_item'
 require 'rails_helper'
 
 describe CreateItem do
+  let(:current_user) { User.create(username: "dreamteam", email: "us@dream.com", password_hash: "zzzzzzz") }
+  let(:tag_names)   { ["fun", "agile"] }
+  let(:item_params) { {name: "ping-pong paddle",
+                       description: "you beat Alex with it",
+                       buy_it_now_price: 1,
+                       seller_id: current_user.id} }
+  let(:invalid_item_params) { {description: "you beat Alex with it",
+                          buy_it_now_price: 1,
+                          seller_id: current_user.id} }
+
   context "item is created succesfully" do
-    let(:current_user) { User.create(username: "dreamteam", email: "us@dream.com", password_hash: "zzzzzzz") }
-    let(:item_params) { {name: "ping-pong paddle",
-                         description: "you beat Alex with it",
-                         buy_it_now_price: 1,
-                         seller_id: current_user.id} }
-    let(:tag_names)   { ["fun", "agile"] }
     let(:create_item) { CreateItem.new(item_params, current_user, tag_names) }
 
     it "returns the item" do
@@ -32,21 +36,16 @@ describe CreateItem do
   end
 
   context "item is not created succesfully" do
-    let(:current_user) { User.create(username: "dreamteam", email: "us@dream.com", password_hash: "zzzzzzz") }
-    let(:item_params) { {description: "you beat Alex with it",
-                         buy_it_now_price: 1,
-                         seller_id: current_user.id} }
-    let(:tag_names)   { ["fun", "agile"] }
-    let(:create_item) { CreateItem.new(item_params, current_user, tag_names) }
+    let(:create_invalid_item) { CreateItem.new(invalid_item_params, current_user, tag_names) }
 
     it "return nil upon failing to create the item" do
-      item = create_item.call
+      item = create_invalid_item.call
 
       expect(item.valid?).to be false
     end
 
     it "does not save item to database" do
-      create_item.call
+      create_invalid_item.call
 
       expect(Item.count).to eq(0)
     end
